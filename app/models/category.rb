6be1b8e68d1ce3e0236cbff548e1be8f0cb85e7a
nissy456789 <i966.326@icloud.com>
2024-11-27 04:43:45 +0900

    中間テーブルとアソシエーション

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,3 @@
 class Category < ApplicationRecord
+  has_many :movie_categories
 end
---
+++ db/migrate/20241126093036_create_movie_categories.rb
@@ -0,0 +1,10 @@
+class CreateMovieCategories < ActiveRecord::Migration[7.1]
+  def change
+    create_table :movie_categories do |t|
+      t.references :movie, foreign_key: true
+      t.references :categorys, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
