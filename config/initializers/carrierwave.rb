e53d834c6bd69e6073168d4f0d99821e7965f8dc
nissy456789 <i966.326@icloud.com>
2024-12-11 11:40:45 +0900

    fix画像表示

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,14 +3,14 @@ require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
 
 CarrierWave.configure do |config|
-    config.storage :fog
-    config.fog_provider = 'fog/aws'
-    config.fog_directory  = 'abchorror ' # バケット名
-    config.fog_credentials = {
-      provider: 'AWS',
-      aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'], # 環境変数
-      aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'], # 環境変数
-      region: 'ap-northeast-1', # リージョン
-      path_style: true
-    }
-end 
+  config.fog_provider = 'fog/aws'                                # fog-aws を使用
+  config.fog_credentials = {
+    provider:              'AWS',                               # 必須
+    aws_access_key_id:     ENV['AWS_ACCESS_KEY_ID'],            # 環境変数から取得
+    aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],        # 環境変数から取得
+    region:                'ap-northeast-1'                     # S3のリージョン
+  }
+  config.fog_directory  = ENV['AWS_S3_BUCKET_NAME']             # バケット名
+  config.fog_public     = false                                # 公開アクセス設定
+  config.asset_host     = ENV['AWS_S3_ASSET_HOST']             # 必要に応じて設定
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,40 +1,19 @@
-# frozen_string_literal: true
-
-
-
-# 映画データ作成と画像のアタッチ
 movie = Movie.create(
   title: 'ウィリーズ・ワンダーランド',
-  description: '車の修理費を稼ぐため、廃れたテーマパークでの清掃を引き受けた男に待ちける悪夢のアニマトロニクス。ニコラス・ケイジが遊園地の人形と死闘を繰り広げるホラー・アクション！'
+  description: '車の修理費を稼ぐため、廃れたテーマパークでの清掃を引き受けた男に待ちける悪夢のアニマトロニクス。ニコラス・ケイジが遊園地の人形と死闘を繰り広げるホラー・アクション！',
+  avatar: File.open(Rails.root.join('app/assets/images/willyswonderland.jpg'))
 )
-movie.images.attach([
-  {
-    io: File.open(Rails.root.join('app/assets/images/willyswonderland.jpg')),
-    filename: 'willyswonderland.jpg',
-    content_type: 'image/jpeg'
-  }
-])
 
 movie = Movie.create(
   title: 'ロンドンゾンビ紀行',
-  description: 'ロンドンに暮らすお気楽なボンクラ兄弟。大好きな祖父が入居している老人ホームが閉鎖の危機にあると知り、ひと働きして助けてあげようと仲間と銀行強盗を企てる。だが、街にはいつの間にかゾンビが溢れており、老人ホームでも入居者たちが迫りくるゾンビを相手に奮闘していた。'
+  description: 'ロンドンに暮らすお気楽なボンクラ兄弟。大好きな祖父が入居している老人ホームが閉鎖の危機にあると知り、ひと働きして助けてあげようと仲間と銀行強盗を企てる。だが、街にはいつの間にかゾンビが溢れており、老人ホームでも入居者たちが迫りくるゾンビを相手に奮闘していた。',
+  avatar: File.open(Rails.root.join('app/assets/images/londonzombiposter.jpg'))
 )
-movie.images.attach([
-  {
-    io: File.open(Rails.root.join('app/assets/images/londonzombiposter.jpg')),
-    filename: 'londonzombiposter.jpg',
-    content_type: 'image/jpeg'
-  }
-])
+
 
 movie = Movie.create(
   title: '黄龍の村',
-  description: 'これ、村の決まりやから。キャンプ場への道中携帯も繋がらない山中で車がパンクし助けを求めて迷い歩く若者達がたどり着いた「切龍村」その近妙な村を舞台に狂気の集団と若者たちの想像を絶する惨劇が勃発するスリラーホラー'
+  description: 'これ、村の決まりやから。キャンプ場への道中携帯も繋がらない山中で車がパンクし助けを求めて迷い歩く若者達がたどり着いた「切龍村」その近妙な村を舞台に狂気の集団と若者たちの想像を絶する惨劇が勃発するスリラーホラー',
+  avatar: File.open(Rails.root.join('app/assets/images/kiryuunomura.jpg'))
 )
-movie.images.attach([
-  {
-    io: File.open(Rails.root.join('app/assets/images/kiryuunomura.jpg')),
-    filename: 'kiryuunomura.jpg',
-    content_type: 'image/jpeg'
-  }
-])
+
