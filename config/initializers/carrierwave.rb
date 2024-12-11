83976981722b75b0257167339156819fd5372ea3
nissy456789 <i966.326@icloud.com>
2024-12-11 12:07:07 +0900

    fix：画像表示！

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -8,9 +8,9 @@ CarrierWave.configure do |config|
     provider:              'AWS',                               # 必須
     aws_access_key_id:     ENV['AWS_ACCESS_KEY_ID'],            # 環境変数から取得
     aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],        # 環境変数から取得
-    region:                'ap-northeast-1'                     # S3のリージョン
+    region:                ENV['AWS_REGION']                   # S3のリージョン
   }
-  config.fog_directory  = ENV['AWS_S3_BUCKET_NAME']             # バケット名
-  config.fog_public     = false                                # 公開アクセス設定
-  config.asset_host     = ENV['AWS_S3_ASSET_HOST']             # 必要に応じて設定
+  config.fog_directory  = ENV['AWS_BUCKET_NAME']                # バケット名
+  config.fog_public     = false                                 # 公開アクセス設定
+  config.asset_host     = ENV['AWS_S3_ASSET_HOST']              # 必要に応じて設定
 end
