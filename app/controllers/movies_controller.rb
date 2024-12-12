dd6aaf773e0c3261bb43bf66071688025c53f0ed
nissy456789 <i966.326@icloud.com>
2024-12-12 16:36:51 +0900

    remove:重複を削除

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,20 +1,8 @@
 class MoviesController < ApplicationController
   def index
-    @movies = Movie.all#Moviesモデルから映画情報を取得する
-
+    @movies = Movie.select(:id, :avatar) # id と avatar カラムだけを取得
   end
-  def create
-    @movie = Movie.new(movie_params)
-    if @movie.save
-      redirect_to @movie, notice: 'Movie was successfully created.'
-    else
-      render :new
-    end
-  end
-
-  private
-
-  def movie_params
-    params.require(:movie).permit(:title, :description, :avatar)
+  def show
+    @movie = Movie.find(params[:id])
   end
 end
