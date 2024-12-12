ad86f766f9d70c7a3841fc76212972c5f900dde9
nissy456789 <i966.326@icloud.com>
2024-12-12 19:08:10 +0900

    fix:重複画像の削除

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,7 +1,8 @@
 class MoviesController < ApplicationController
   def index
-    @movies = Movie.select(:id, :avatar).distinct
+    @movies = Movie.select(:id, :avatar).distinct.limit(3)
   end
+
   def show
     @movie = Movie.find(params[:id])
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,14 @@
-movie = Movie.create(
-  title: 'ウィリーズ・ワンダーランド',
-  description: '車の修理費を稼ぐため、廃れたテーマパークでの清掃を引き受けた男に待ちける悪夢のアニマトロニクス。ニコラス・ケイジが遊園地の人形と死闘を繰り広げるホラー・アクション！',
-  avatar: File.open(Rails.root.join('app/assets/images/willyswonderland.jpg'))
-)
+Movie.find_or_create_by(title: 'ウィリーズ・ワンダーランド') do |movie|
+  movie.description = '車の修理費を稼ぐため、廃れたテーマパークでの清掃を引き受けた男に待ちける悪夢のアニマトロニクス。ニコラス・ケイジが遊園地の人形と死闘を繰り広げるホラー・アクション！'
+  movie.avatar = File.open(Rails.root.join('app/assets/images/willyswonderland.jpg'))
+end
 
-movie = Movie.create(
-  title: 'ロンドンゾンビ紀行',
-  description: 'ロンドンに暮らすお気楽なボンクラ兄弟。大好きな祖父が入居している老人ホームが閉鎖の危機にあると知り、ひと働きして助けてあげようと仲間と銀行強盗を企てる。だが、街にはいつの間にかゾンビが溢れており、老人ホームでも入居者たちが迫りくるゾンビを相手に奮闘していた。',
-  avatar: File.open(Rails.root.join('app/assets/images/londonzombiposter.jpg'))
-)
+Movie.find_or_create_by(title: 'ロンドンゾンビ紀行') do |movie|
+  movie.description = 'ロンドンに暮らすお気楽なボンクラ兄弟。大好きな祖父が入居している老人ホームが閉鎖の危機にあると知り、ひと働きして助けてあげようと仲間と銀行強盗を企てる。だが、街にはいつの間にかゾンビが溢れており、老人ホームでも入居者たちが迫りくるゾンビを相手に奮闘していた。'
+  movie.avatar = File.open(Rails.root.join('app/assets/images/londonzombiposter.jpg'))
+end
 
-movie = Movie.create(
-  title: '黄龍の村',
-  description: 'これ、村の決まりやから。キャンプ場への道中携帯も繋がらない山中で車がパンクし助けを求めて迷い歩く若者達がたどり着いた「切龍村」その近妙な村を舞台に狂気の集団と若者たちの想像を絶する惨劇が勃発するスリラーホラー',
-  avatar: File.open(Rails.root.join('app/assets/images/kiryuunomura.jpg'))
-)
+Movie.find_or_create_by(title: '黄龍の村') do |movie|
+  movie.description = 'これ、村の決まりやから。キャンプ場への道中携帯も繋がらない山中で車がパンクし助けを求めて迷い歩く若者達がたどり着いた「切龍村」その近妙な村を舞台に狂気の集団と若者たちの想像を絶する惨劇が勃発するスリラーホラー'
+  movie.avatar = File.open(Rails.root.join('app/assets/images/kiryuunomura.jpg'))
+end
