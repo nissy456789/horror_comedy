375af59ec0f5c9c72f99f30adde188faf3dbbbad
nissy456789 <i966.326@icloud.com>
2024-12-12 11:34:51 +0900

    fix初期画像表示

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -3,4 +3,18 @@ class MoviesController < ApplicationController
     @movies = Movie.all#Moviesモデルから映画情報を取得する
 
   end
+  def create
+    @movie = Movie.new(movie_params)
+    if @movie.save
+      redirect_to @movie, notice: 'Movie was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def movie_params
+    params.require(:movie).permit(:title, :description, :avatar)
+  end
 end
