fa8207ab986fc8af7e98acf42fb2523308583a27
nissy456789 <i966.326@icloud.com>
2024-12-12 18:20:48 +0900

    remove:重複ファイル

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,6 +1,6 @@
 class MoviesController < ApplicationController
   def index
-    @movies = Movie.select(:id, :avatar) # id と avatar カラムだけを取得
+    @movies = Movie.select(:id, :avatar).distinct
   end
   def show
     @movie = Movie.find(params[:id])
