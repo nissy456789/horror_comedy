70d734123fe7fb77957b91903457a225442c3637
nissy456789 <i966.326@icloud.com>
2024-12-13 19:01:21 +0900

    add:レビュー機能

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,6 +1,17 @@
 class ReviewsController < ApplicationController
-  def create
-    
+  def create#レビュー作成
+    review = Movie.reviews.build(review_params)
+    if review.save
+      redirect_to movie_path(review.movie), success: '投稿してくれてありがとう！', item: Review.model_name.human)
+    else
+      redirect_to movie_path(review.movie), danger: 'ごめん！投稿できてないみたい！', item: Review.model_name.human)
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:content).merge(movie_id: params[:movie_id])
   end
 
 end
