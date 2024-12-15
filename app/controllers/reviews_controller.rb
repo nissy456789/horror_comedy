c8e4e13c87f553f5945fbd9c4086c6d8606bc176
nissy456789 <i966.326@icloud.com>
2024-12-15 16:52:59 +0900

    update:レビュー投稿

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,10 +1,10 @@
 class ReviewsController < ApplicationController
   def create#レビュー作成
-    review = Movie.reviews.build(review_params)
+    review = current_user.reviews.build(review_params)
     if review.save
-      redirect_to movie_path(review.movie), success: '投稿してくれてありがとう！', item: Review.model_name.human)
+      redirect_to movie_path(review.movie), success: '投稿してくれてありがとう！', item: Review.model_name.human
     else
-      redirect_to movie_path(review.movie), danger: 'ごめん！投稿できてないみたい！', item: Review.model_name.human)
+      redirect_to movie_path(review.movie), danger: 'ごめん！投稿できてないみたい！', item: Review.model_name.human
     end
   end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,12 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :reviews
+
+  #自分のレビューか確認する
+  def own?(object)
+    id == object&.user_id
+  end
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
