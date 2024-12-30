972520f212e04a553a2455f8ce904ad5f9125a64
nissy456789 <i966.326@icloud.com>
2024-12-30 11:15:15 +0900

    add:tmdb-api取得

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,6 +1,6 @@
 class MoviesController < ApplicationController
   require 'themoviedb-api'
-  Tmdb::Api.key(841e56c5caeb10709f58e685aef48333)
+  Tmdb::Api.key("841e56c5caeb10709f58e685aef48333")
   Tmdb::Api.language("ja") # こちらで映画情報の表示の際の言語設定を日本語にできます
 
   def search
