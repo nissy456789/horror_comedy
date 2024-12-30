d655157f28c8ab221fdd3bfccb46cd59a4d9f2f4
nissy456789 <i966.326@icloud.com>
2024-12-30 10:34:09 +0900

    add:tmdb-api取得

--- Gemfile
+++ Gemfile
@@ -63,6 +63,8 @@ gem 'dotenv-rails'
 
 gem 'fog-aws'
 
+gem 'themoviedb-api', '~> 1.4', '>= 1.4.1'
+
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem 'debug', platforms: %i[mri windows]
--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,4 +1,11 @@
 class MoviesController < ApplicationController
+  require 'themoviedb-api'
+  Tmdb::Api.key(841e56c5caeb10709f58e685aef48333)
+  Tmdb::Api.language("ja") # こちらで映画情報の表示の際の言語設定を日本語にできます
+
+  def search
+  end
+
   def index
     @movies = Movie.select(:id, :avatar).distinct.limit(3)
   end
