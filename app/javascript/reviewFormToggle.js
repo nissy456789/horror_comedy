31024eb2dd8c5c2dbeb85550950b04a5a74f1133
nissy456789 <i966.326@icloud.com>
2025-01-24 14:26:06 +0900

    画像調整

--- app/javascript/reviewFormToggle.js
+++ app/javascript/reviewFormToggle.js
@@ -1,6 +1,4 @@
-<script>
-  document.getElementById('toggle-form').onclick = function() {
-      var formDropdown = document.getElementById('review-form-dropdown');
-      formDropdown.classList.toggle('hidden');
-  };
-</script>
+document.getElementById('toggle-form').onclick = function() {
+    var formDropdown = document.getElementById('review-form-dropdown');
+    formDropdown.classList.toggle('hidden');
+};
