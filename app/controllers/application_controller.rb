8d24411a2220c3938910e93ae3c53b0bc18e6ef6
nissy456789 <i966.326@icloud.com>
2025-02-15 08:52:36 +0900

    add:Google認証

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 #デバイスのストロングパラメーターにnameの保存を許可する
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :client_id ]) # 新規登録時(sign_up時)にnameというキーのパラメーターを追加で許可する
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :client_id]) # 新規登録時(sign_up時)にnameというキーのパラメーターを追加で許可する
   end
 
   
