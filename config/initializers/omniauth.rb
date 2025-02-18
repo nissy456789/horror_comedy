1178133009bf4801081b94e05510be232c8536a0
nissy456789 <i966.326@icloud.com>
2025-02-18 21:52:35 +0900

    fix Google認証できない

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -4,7 +4,8 @@ Rails.application.config.middleware.use OmniAuth::Builder do
         prompt: 'select_account',
         image_aspect_ratio: 'square',
         image_size: 50,
-        access_type: 'offline'
+        access_type: 'offline',
+        redirect_uri: 'https://horror-comedy.onrender.com/users/auth/google_oauth2/callback'
       }
   end
   OmniAuth.config.allowed_request_methods = [:post, :get]
