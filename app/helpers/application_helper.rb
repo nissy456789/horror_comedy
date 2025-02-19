f3a73ddb874157deff5829b67026414072364fc5
nissy456789 <i966.326@icloud.com>
2025-02-19 20:22:33 +0900

    fix: 本版環境のURL修正

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,14 +9,14 @@ module ApplicationHelper
       charset: 'utf-8',
       description: 'Live Fesでは、音楽ライブやフェスの余韻や喪失感を参加者同士で共通し、感想や思い出を語り合うことができます。',
       keywords: '音楽,ライブ,フェス,余韻,喪失感,共有',
-      canonical: https://horror-comedy.onrender.com,
+      canonical: 'https://horror-comedy.onrender.com',
       separator: '|',
       og: {
       site_name: 'Live Fes',
       title: '音楽ライブ・フェスの余韻を共有できるサービス',
       description: 'Live Fesでは、音楽ライブやフェスの余韻や喪失感を参加者同士で共通し、感想や思い出を語り合うことができます。',
       type: 'website',
-      url: https://horror-comedy.onrender.com,
+      canonical: 'https://horror-comedy.onrender.com',
       image: image_url('komitera.jpg'),
       locale: 'ja_JP'
     },
