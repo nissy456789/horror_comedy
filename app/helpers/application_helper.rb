2f485276fc707b26d45bc19132789e31d1ad9869
nissy456789 <i966.326@icloud.com>
2025-02-22 14:54:41 +0900

    add:Xシェア

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,34 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
-
+    def default_meta_tags
+        {
+          site: 'こみてらっ！',
+          title: 'B級ホラー映画',
+          reverse: true,
+          charset: 'utf-8',
+          description: '初心者向けの笑えるホラー映画を紹介しています',
+          keywords: 'ホラー,恐怖,初心者,スリル',
+          canonical: 'https://horror-comedy.onrender.com',  # 修正が必要
+          separator: '|',
+          og: {
+            site_name: :site,
+            title: 'こみてらっ！',  # ページ固有のタイトル
+            description: '怖くないB級ホラー映画紹介アプリです',  # ページ固有の説明
+            type: 'website',
+            url: request.original_url,  # 現在のページのURLを取得
+            image: image_url('komitera2.jpg'),  # ページ固有の画像
+            local: 'ja-JP'
+          },
+          twitter: {
+            card: 'summary_large_image',
+            site: '@obvyamdrss',
+            title: 'こみてらっ！',  # ページ固有のTwitterタイトル
+            description: '怖くないB級ホラー映画紹介アプリです#ホラー映画',  # ページ固有のTwitter説明
+            image: image_url('komitera2.jpg'),  # ページ固有のTwitter画像
+            url: request.original_url  # 現在のページのURLを取得
+          }
+        }
+      end
+      
 end
