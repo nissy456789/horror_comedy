3226b7dac0be95c97118f76f0ae1c350cfe75fe5
nissy456789 <i966.326@icloud.com>
2025-04-11 22:41:10 +0900

    fix:ユーザーへ詳細画面の見た目修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :client_id])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :profile])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
   end
 
   
