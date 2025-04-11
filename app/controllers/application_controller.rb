0cf0dee59efcd43ee516d1741cd1cb086bd2b460
nissy456789 <i966.326@icloud.com>
2025-04-11 14:22:29 +0900

    add:ユーザー情報編集

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,10 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-#deviceのコントローラーを使用しているときに処理される
   before_action :configure_permitted_parameters, if: :devise_controller?
-#デバイスのストロングパラメーターにnameの保存を許可する
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :client_id]) # 新規登録時(sign_up時)にnameというキーのパラメーターを追加で許可する
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :client_id])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :profile])
   end
 
   
